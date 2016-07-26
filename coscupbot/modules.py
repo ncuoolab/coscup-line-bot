fedc2bc34085d5b9b556212a06a62ba720cbee6f
eternnoir <eternnoir@gmail.com>
2016-07-26 23:06:36 +0800

    Add  command class.

---
+++ coscupbot/modules.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from coscupbot import api, db, module
+
+class MessageController(object):
+    def __init__(self, bot_api, db_url):
+        self.bot_api = bot_api
+        self.dao = db.Dao(db_url)
