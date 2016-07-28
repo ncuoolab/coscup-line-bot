fb5a9e207b9d9ff135332a10c9fb204a8403704e
eternnoir <eternnoir@gmail.com>
2016-07-28 11:04:56 +0800

    Add Nlp Action model.

--- coscupbot/model.py
+++ coscupbot/model.py
@@ -14,6 +14,19 @@ class Command(object):
         self.response = response
 
 
+class NlpAction(object):
+    def __init__(self, language, action_str, responses):
+        """
+        Command object.
+        :param language: language set. eg. zh_TW
+        :param command_str: string to trigger command. eg. help
+        :param response: list of response content. eg. ['hi', 'hello']
+        """
+        self.action_str = action_str
+        self.language = language
+        self.response = responses
+
+
 class NLPActions(object):
     Welcome = 'WELCOME'
     Location = 'LOCATION'
