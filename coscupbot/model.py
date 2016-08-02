8535710949ed0df3a4dc710571ecf5e0a6903227
eternnoir <eternnoir@gmail.com>
2016-08-02 18:49:59 +0800

    All api's model done.

--- coscupbot/model.py
+++ coscupbot/model.py
@@ -132,11 +132,11 @@ class ProgramType(object):
         jsonobj = check_json(json_str)
         ret = []
         for program_json in jsonobj:
-            ret.append(ProgramType.de_json_program_type(program_json))
+            ret.append(ProgramType.de_json(program_json))
         return ret
 
     @classmethod
-    def de_json_program_type(cls, json_obj):
+    def de_json(cls, json_obj):
         type = json_obj.get('type')
         nameen = json_obj.get('nameen')
         namezh = json_obj.get('namezh')
@@ -148,4 +148,89 @@ class ProgramType(object):
         self.name_zh = name_zh
 
 
+class Sponsor(object):
+    @classmethod
+    def de_json_list(cls, json_str):
+        jsonobj = check_json(json_str)
+        ret = []
+        for sponsor_json in jsonobj:
+            ret.append(Sponsor.de_json(sponsor_json))
+        return ret
 
+    @classmethod
+    def de_json(cls, json_obj):
+        level = json_obj.get('level')
+        logolink = json_obj.get('logolink')
+        logourl = json_obj.get('logourl')
+        name_en = json_obj.get('nameen')
+        name_zh = json_obj.get('namezh')
+        intro_en = json_obj.get('introen')
+        intro_zh = json_obj.get('introzh')
+        return Sponsor(level, logolink, logourl, name_en, name_zh, intro_en, intro_zh)
+
+    def __init__(self, level, place, logolink, logourl, name_en, name_zh, intro_en, intro_zh):
+        self.level = level
+        self.place = place
+        self.logolink = logolink
+        self.logourl = logourl
+        self.name_en = name_en
+        self.name_zh = name_zh
+        self.intro_en = intro_en
+        self.intro_zh = intro_zh
+
+
+class Level(object):
+    @classmethod
+    def de_json_list(cls, json_str):
+        jsonobj = check_json(json_str)
+        ret = []
+        for level_json in jsonobj:
+            ret.append(Level.de_json(level_json))
+        return ret
+
+    @classmethod
+    def de_json(cls, json_obj):
+        level = json_obj.get('level')
+        name_en = json_obj.get('nameen')
+        name_zh = json_obj.get('namezh')
+        return Level(level, name_en, name_zh)
+
+    def __init__(self, level, name_en, name_zh):
+        self.level = level
+        self.name_zh = name_zh
+        self.name_en = name_en
+
+
+class Transport(object):
+    @classmethod
+    def de_json(cls, json_str):
+        """
+        This class's datastruct totally nightmare. Never parse it.
+        :param json_str:
+        :return:
+        """
+        jsonobj = check_json(json_str)
+        return Transport(jsonobj)
+
+    def __init__(self, jsonobj):
+        self.json_obj = jsonobj
+
+
+class Staff(object):
+    @classmethod
+    def de_json_list(cls, json_str):
+        jsonobj = check_json(json_str)
+        ret = []
+        for staff_json in jsonobj:
+            ret.append(Staff.de_json(staff_json))
+        return ret
+
+    @classmethod
+    def de_json(cls, json_obj):
+        team = json_obj.get('team')
+        members = json_obj.get('members')
+        return Staff(team, members)
+
+    def __init__(self, team, members):
+        self.team = team
+        self.members = members
