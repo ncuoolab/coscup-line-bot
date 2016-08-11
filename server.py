5771edf76336f4974bc9d34ef8d55c36bbc90de4
hinet60613 <hinet60613@gmail.com>
2016-08-11 23:49:42 +0800

    add check_in template

--- server.py
+++ server.py
@@ -182,7 +182,7 @@ def sp_with_id(sp_id):
 @app.route('/sp/<sp_id>/<mid>')
 def sp_check_in(sp_id, mid):
     ret = bot.ground_game_check_in(sp_id, mid)
-    return render_template('check_in.html', ret)
+    return render_template('check_in.html', check_in_data=ret)
 
 if __name__ == '__main__':
     app.run()
