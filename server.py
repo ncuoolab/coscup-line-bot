91f11cef5d0bcc43b27db3e26e3366ee099dbd3c
hinet60613 <hinet60613@gmail.com>
2016-08-19 01:07:16 +0800

    replace key to booth id

--- server.py
+++ server.py
@@ -197,8 +197,9 @@ def sp_with_id(sp_id):
 def sp_check_in(sp_id, mid):
     ret = bot.ground_game_check_in(sp_id, mid)
     for status in ret['status']:
-        ret['status'][utils.SponsorKeyDic[status]] = ret['status'][status]
-        ret['status'].pop(status)
+        ret['status'][utils.SponsorKeyDic[status]['booth']] = ret['status'][status]
+    for sp_key in utils.SponsorKeyDic:
+        ret['status'].pop(sp_key)
     if 'error' in ret:
         return render_template('check_in_failed.html', err_msg=ret['error'])
     else:
